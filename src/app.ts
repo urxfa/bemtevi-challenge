05f14589d4e39a8f448a3dacfccc10c63fe47f7f
AF <af.arthurfaria@gmail.com>
2024-11-27 15:04:30 -0300

    export app as handler for vercel

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
-import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { cors } from 'hono/cors';
 
 import routes from './routes/index.ts';
 
-import { logger } from 'hono/logger'
-
 const app = routes;
-const port = 3000;
 
-app.use(logger())
+app.use('*', logger());
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:5173', 'https://bemtevi-challenge-frontend.vercel.app'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
-app.get('/', async c => {
-  return c.text("Funcionando!!")
-})
+app.get('/', (c) => c.text('Funcionando!!'));
 
-serve({
-  fetch: app.fetch,
-  port,
-});
-
+export default app;
